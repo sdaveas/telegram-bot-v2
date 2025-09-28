40ec69fd08b7b2f7e7b438037b036d3fe76cebf7
Stelios Daveas <sdaveas@gmail.com>
2025-09-28 15:48:00 +0300

    chore: add script to remove trailing whitespaces

---
+++ scripts/remove_whitespaces.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import os
+import glob
+
+def remove_trailing_whitespace(file_path):
+    """Remove trailing whitespace from a file."""
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    # Remove trailing whitespace from each line
+    cleaned_lines = [line.rstrip() + '\n' for line in lines]
+
+    # Remove any trailing newlines at the end of the file
+    while cleaned_lines and cleaned_lines[-1] == '\n':
+        cleaned_lines.pop()
+    if cleaned_lines:
+        cleaned_lines.append('\n')
+
+    with open(file_path, 'w') as f:
+        f.writelines(cleaned_lines)
+
+def main():
+    # Get all Python files recursively
+    python_files = []
+    for root, _, _ in os.walk('.'):
+        if 'venv' in root or '.git' in root:
+            continue
+        python_files.extend(glob.glob(os.path.join(root, '*.py')))
+
+    # Process each file
+    for file_path in python_files:
+        print(f'Processing {file_path}')
+        remove_trailing_whitespace(file_path)
+
+if __name__ == '__main__':
+    main()
+
